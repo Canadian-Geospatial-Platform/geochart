64cf439557c325056118e25bcd32d122b0908afd
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2025-10-06 09:48:50 -0400

    fix all?

--- webpack.common.js
+++ webpack.common.js
@@ -56,11 +56,6 @@ const config = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public/locales', to: 'locales' }, { from: './favicon.ico' }, { from: './public/configs', to: 'configs' }],
     })
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: './public/configs', to: 'configs' }
-      ]
-    })
   ],
 };
 
